88e1693e189179917c242bd8d875c791ba8fbe43
Suniket23 <suniketkhairnar@gmail.com>
2023-02-12 17:20:18 +0530

    login page added

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,8 @@
-$(".btn").click(function(){
-    alert("i got clicked");
+$("#post").click(function(e){
+    e.preventDefault();
+
+	// Replace localhost and the folder name
+	// based on your setup
+	// location.href = 'http://localhost/jsredirect/home.html';
+    location.href='http://127.0.0.1:5500/frontend/login.html';
 })
